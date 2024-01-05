6d4b6d5c694e6692e4efac66999920e825600fef
Nauman3S <naumanshakir3s@gmail.com>
2024-01-05 20:08:50 +0100

    added fn

--- cpp-wrapers/configmap/kube_operations.c
+++ cpp-wrapers/configmap/kube_operations.c
@@ -7,7 +7,7 @@
 #include <unistd.h>
 #include "kube_operations.h"
 
-void create_configmap(apiClient_t * apiClient, char *name, char *namespace_)
+void create_configmap(apiClient_t *apiClient, char *name, char *namespace_)
 {
     char *api_version = strdup("v1");
     char *kind = strdup("ConfigMap");
@@ -51,130 +51,178 @@ void create_configmap(apiClient_t * apiClient, char *name, char *namespace_)
 
     printf("%s: The return code of HTTP request=%ld\n", __func__, apiClient->response_code);
 
-    if (201 == apiClient->response_code) {
+    if (201 == apiClient->response_code)
+    {
         printf("%s: Create the config map successfully.\n", __func__);
-    } else {
+    }
+    else
+    {
         fprintf(stderr, "%s: Failed to create the config map.\n", __func__);
         return;
     }
 
-    if (ret_config_map) {
+    if (ret_config_map)
+    {
         v1_config_map_free(ret_config_map);
         ret_config_map = NULL;
     }
-    if (body) {
+    if (body)
+    {
         v1_config_map_free(body);
         body = NULL;
     }
 }
 
-void list_configmap(apiClient_t * apiClient, char *namespace_)
+void list_configmap(apiClient_t *apiClient, char *namespace_)
 {
     v1_config_map_list_t *config_map_list = CoreV1API_listNamespacedConfigMap(apiClient,
-                                                                              namespace_,   // char *namespace
-                                                                              "true",   // char *pretty
-                                                                              NULL, // int *allowWatchBookmarks
-                                                                              NULL, // char *_continue
-                                                                              NULL, // char *fieldSelector
-                                                                              NULL, // char *labelSelector
-                                                                              NULL, // int *limit
-                                                                              NULL, // char *resourceVersion
-                                                                              NULL, // char *resourceVersionMatch
-                                                                              NULL, // sendInitialEvents
-                                                                              NULL, // int *timeoutSeconds
-                                                                              NULL  //int *watch
-        );
+                                                                              namespace_, // char *namespace
+                                                                              "true",     // char *pretty
+                                                                              NULL,       // int *allowWatchBookmarks
+                                                                              NULL,       // char *_continue
+                                                                              NULL,       // char *fieldSelector
+                                                                              NULL,       // char *labelSelector
+                                                                              NULL,       // int *limit
+                                                                              NULL,       // char *resourceVersion
+                                                                              NULL,       // char *resourceVersionMatch
+                                                                              NULL,       // sendInitialEvents
+                                                                              NULL,       // int *timeoutSeconds
+                                                                              NULL        // int *watch
+    );
 
     printf("%s: The return code of HTTP request=%ld\n", __func__, apiClient->response_code);
 
-    if (200 == apiClient->response_code) {
+    if (200 == apiClient->response_code)
+    {
         printf("%s: List the config maps successfully.\n", __func__);
-    } else {
+    }
+    else
+    {
         fprintf(stderr, "%s: Failed to list the config maps.\n", __func__);
         return;
     }
 
-    if (config_map_list && config_map_list->items) {
+    if (config_map_list && config_map_list->items)
+    {
         listEntry_t *config_map_list_entry = NULL;
         v1_config_map_t *config_map = NULL;
-        list_ForEach(config_map_list_entry, config_map_list->items) {
+        list_ForEach(config_map_list_entry, config_map_list->items)
+        {
             config_map = config_map_list_entry->data;
             printf("\tThe config map name: %s\n", config_map->metadata->name);
 
             listEntry_t *data_entry = NULL;
             keyValuePair_t *pair = NULL;
-            list_ForEach(data_entry, config_map->data) {
+            list_ForEach(data_entry, config_map->data)
+            {
                 pair = data_entry->data;
-                printf("\tkey=%s, value=%s\n", pair->key, (char *) pair->value);
+                printf("\tkey=%s, value=%s\n", pair->key, (char *)pair->value);
             }
         }
         v1_config_map_list_free(config_map_list);
         config_map_list = NULL;
-    } else {
+    }
+    else
+    {
         fprintf(stderr, "%s: The config map list is invalid.\n", __func__);
     }
 }
 
-void delete_configmap(apiClient_t * apiClient, char *name, char *namespace_)
+void delete_configmap(apiClient_t *apiClient, char *name, char *namespace_)
 {
     v1_status_t *status = CoreV1API_deleteNamespacedConfigMap(apiClient,
-                                                              name, // char *name
-                                                              namespace_,   // char *namespace
-                                                              NULL, // char *pretty
-                                                              NULL, // char *dryRun
-                                                              NULL, // int *gracePeriodSeconds
-                                                              NULL, // int *orphanDependents
-                                                              NULL, // char *propagationPolicy
-                                                              NULL  // v1_delete_options_t *body
-        );
+                                                              name,       // char *name
+                                                              namespace_, // char *namespace
+                                                              NULL,       // char *pretty
+                                                              NULL,       // char *dryRun
+                                                              NULL,       // int *gracePeriodSeconds
+                                                              NULL,       // int *orphanDependents
+                                                              NULL,       // char *propagationPolicy
+                                                              NULL        // v1_delete_options_t *body
+    );
 
     printf("The return code of HTTP request=%ld\n", apiClient->response_code);
 
-    if (200 == apiClient->response_code || 202 == apiClient->response_code) {
+    if (200 == apiClient->response_code || 202 == apiClient->response_code)
+    {
         printf("The config map is deleted successfully.\n");
-    } else {
-        if (status && status->message) {
+    }
+    else
+    {
+        if (status && status->message)
+        {
             printf("Failed to delete the config map. The error message: %s\n", status->message);
         }
     }
 
-    if (status) {
+    if (status)
+    {
         v1_status_free(status);
         status = NULL;
     }
 }
 
-int main(int argc, char *argv[])
+int initialize_kube(apiClient_t **apiClient, char **basePath, sslConfig_t **sslConfig, list_t **apiKeys)
 {
-    char *basePath = NULL;
-    sslConfig_t *sslConfig = NULL;
-    list_t *apiKeys = NULL;
-    int rc = load_kube_config(&basePath, &sslConfig, &apiKeys, NULL);   /* NULL means loading configuration from $HOME/.kube/config */
-    if (rc != 0) {
+    *apiClient = NULL; // Initialize output pointers to NULL
+    *basePath = NULL;
+    *sslConfig = NULL;
+    *apiKeys = NULL;
+
+    int rc = load_kube_config(basePath, sslConfig, apiKeys, NULL); /* NULL means loading configuration from $HOME/.kube/config */
+    if (rc != 0)
+    {
         fprintf(stderr, "Cannot load kubernetes configuration.\n");
-        return -1;
+        return -1; // Return an error code
     }
-    apiClient_t *apiClient = apiClient_create_with_base_path(basePath, sslConfig, apiKeys);
-    if (!apiClient) {
+
+    *apiClient = apiClient_create_with_base_path(*basePath, *sslConfig, *apiKeys);
+    if (!*apiClient)
+    {
         fprintf(stderr, "Cannot create a kubernetes client.\n");
-        return -1;
+        return -1; // Return an error code
     }
 
-    char *config_map_name = "cm1";
-    char *namespace_ = "default";
-    create_configmap(apiClient, config_map_name, namespace_);
-    sleep(5);
-    list_configmap(apiClient, namespace_);
-    sleep(5);
-    delete_configmap(apiClient, config_map_name, namespace_);
+    return 0; // Return success
+}
+void perform_configmap_operations(apiClient_t *apiClient, ConfigMapOperation op, const char *config_map_name, const char *namespace_)
+{
 
-    apiClient_free(apiClient);
-    apiClient = NULL;
-    free_client_config(basePath, sslConfig, apiKeys);
-    basePath = NULL;
-    sslConfig = NULL;
-    apiKeys = NULL;
-    apiClient_unsetupGlobalEnv();
+    // char *config_map_name = "cm1";
+    // char *namespace_ = "default";
+    if (apiClient == NULL)
+    {
+        fprintf(stderr, "Invalid API client. Initialization required.\n");
+        return;
+    }
+    switch (op)
+    {
+    case CREATE:
+        create_configmap(apiClient, config_map_name, namespace_);
+        break;
+    case DELETE:
+        // call delete_configmap function here
+        delete_configmap(apiClient, config_map_name, namespace_);
+
+        break;
+    case LIST:
+        // call list_configmap function here
+        list_configmap(apiClient, namespace_);
+        break;
+    default:
+        fprintf(stderr, "Invalid operation specified.\n");
+    }
 
     return 0;
+}
+
+int free_kube(apiClient_t **apiClient, char **basePath, sslConfig_t **sslConfig, list_t **apiKeys)
+{
+    apiClient_free(apiClient);
+    *apiClient = NULL;
+    free_client_config(basePath, sslConfig, apiKeys);
+    *basePath = NULL;
+    *sslConfig = NULL;
+    *apiKeys = NULL;
+    apiClient_unsetupGlobalEnv();
 }
--- cpp-wrapers/configmap/kube_operations.h
+++ cpp-wrapers/configmap/kube_operations.h
@@ -1,10 +1,29 @@
 #ifndef KUBE_OPERATIONS_H
 #define KUBE_OPERATIONS_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <apiClient.h>
 
-void create_configmap(apiClient_t *apiClient, char *name, char *namespace_);
-void list_configmap(apiClient_t *apiClient, char *namespace_);
-void delete_configmap(apiClient_t *apiClient, char *name, char *namespace_);
+// Enum for different config map operations
+typedef enum {
+    CREATE,
+    DELETE,
+    LIST
+} ConfigMapOperation;
+
+// Function to initialize and return a Kubernetes API client
+int initialize_kube(apiClient_t **apiClient, char **basePath, sslConfig_t **sslConfig, list_t **apiKeys);
+
+// Function to perform specified operation on config maps
+void perform_configmap_operations(apiClient_t* apiClient, const char *namespace_, ConfigMapOperation op);
+
+int free_kube(apiClient_t **apiClient, char **basePath, sslConfig_t **sslConfig, list_t **apiKeys);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // KUBE_OPERATIONS_H
